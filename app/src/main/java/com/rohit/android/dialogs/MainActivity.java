93a5b6ce43cd77596e694b2f4cdc66462b5bea6e
Rohit Kumar <rohit9075@gmail.com>
2018-07-24 11:25:23 +0530

    comment

--- app/src/main/java/com/rohit/android/dialogs/MainActivity.java
+++ app/src/main/java/com/rohit/android/dialogs/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    // method to show the dialog.
     private void showDialog() {
 
         // creating the object of the AlertDialogFragment
